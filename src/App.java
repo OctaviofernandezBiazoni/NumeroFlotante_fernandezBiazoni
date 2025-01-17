45bd4756f600d5609e95bd22af96c127a3e67f64
octavio fernandez biazoni <octaviobiazoni@gmail.com>
2025-01-16 22:49:03 -0300

    2numeroFlotante_FernandezBiazoni

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        /*declaro una variable float*/
+        float numero = 123.456f;
+       
+       /*declaro una variable de tipo int, realizo el parseo 
+        de datos de float a int y le asigno la parte entera*/
+       int parteEntera = (int) numero;
+       
+       /*declaro una variable de tipo float, realizo el parseo 
+        de datos de float a float y le asigno la parte decimal*/
+       float parteDecimal = numero - parteEntera;
+       
+       /*imprimo por consola los datos obtenidos*/
+       System.out.println("numero:" + numero);
+       System.out.println("parteEntera:" + parteEntera);
+       System.out.println("parteDecimal:" + parteDecimal);
+    }
+}
