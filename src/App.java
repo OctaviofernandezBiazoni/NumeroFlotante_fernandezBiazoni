4af641ffcb521e283484a1e638ec5a7bab7c8428
octavio fernandez biazoni <octaviobiazoni@gmail.com>
2025-01-16 22:53:01 -0300

    modificacion de prueba

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
        
         /*declaro una variable float*/
-        float numero = 123.456f;
+        float numero = 27.56f;
        
        /*declaro una variable de tipo int, realizo el parseo 
         de datos de float a int y le asigno la parte entera*/
